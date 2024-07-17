07a6dca49a3b48bef0c3459912cf62097a357ba0
gulnur <abdrashitovagulnur05@gmail.com>
2024-07-17 15:29:00 +0300

    finished home-work-2

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let firstUserName = " Nooruz ";
+let secondUserName = " Zhakshylyk";
+let thirdUserName = "Alisher";
+let fourthUserName = "Elizar";
+let firstUserPoint = 97;
+let secondUserPoint = 89;
+let thirdUserPoint = 75;
+let fourthUserPoint = 90;
+
+
+
+console.log(`1. ${firstUserName} - ${97} баллов из 100`);
+console.log(`2. ${secondUserName} - ${89} баллов из 100`);
+console.log(`3.${thirdUserName} - ${75} баллов из 100`);
+console.log(`4. ${fourthUserName} - ${90} баллов из 100`);
